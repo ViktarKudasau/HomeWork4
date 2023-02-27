945273bfe6f06e52fe294504fae77de14ae9dbc0
ViktarKudasau <viktar.kudasau@gmail.com>
2023-02-28 02:04:46 +0300

    Add Task_1 About myself

---
+++ src/HomeWork_1.java
@@ -0,0 +1,8 @@
+public class HomeWork_1 {
+    public static void main(String[] args) {
+
+        System.out.println("Viktar\nKudasau");
+        System.out.println("Economist");
+        System.out.println("GYM");
+    }
+}
