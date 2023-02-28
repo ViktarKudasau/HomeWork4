4df9b9478d9995c6e59b2a6092bed0f8bc544e36
ViktarKudasau <viktar.kudasau@gmail.com>
2023-02-28 12:54:32 +0300

    Check russian language

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@
 public class Main {
 
     public static void main(String[] args) {
+        String name = "Viktar";
         String Text = "Hello! Русский язык распознаю.";
         System.out.println(Text);
+        System.out.println(String.format("Значение выражения 'name' = %s", name));
     }
 }
