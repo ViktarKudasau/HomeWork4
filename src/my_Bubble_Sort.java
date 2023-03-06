12cd69772bc7ace27b125687519321dca749b84c
ViktarKudasau <viktar.kudasau@gmail.com>
2023-03-06 12:04:07 +0300

    Bubble Sort

---
+++ src/my_Bubble_Sort.java
@@ -0,0 +1,26 @@
+public class my_Bubble_Sort {
+    public static void main(String[] args){
+
+        int[] array = {23,17,34,3,157,18,25,39,163,27,99};
+        PrintArray(array);
+        boolean sort = false;
+        while (sort != true){
+            sort = true;
+            for (int i = 0; i < array.length-1; i++) {
+                if (array[i] > array[i+1]){
+                    int temp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = temp;
+                    sort = false;
+                }
+            }
+        }
+        System.out.println();
+        PrintArray(array);
+    }
+    static void PrintArray(int[] arrayToPrint){
+        for (int i = 0; i < arrayToPrint.length; i++) {
+            System.out.print(" " +arrayToPrint[i]);
+        }
+    }
+}
