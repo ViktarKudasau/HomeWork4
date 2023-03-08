d3b3d8243a2525d97aeba9c2e93a571f545ca159
ViktarKudasau <viktar.kudasau@gmail.com>
2023-03-08 22:30:54 +0300

    Task №4 // метод Random

--- src/RandomCat.java
+++ src/RandomCat.java
@@ -45,5 +45,6 @@ public class RandomCat {
         CatClass cat = new CatClass(getRandomName(), getRandomAge(), getRandomName(), getRandomName(), getRandomBoolean());
         return cat;
     }
+
 }
 
