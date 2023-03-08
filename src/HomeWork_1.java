8aedfde8e345d38e697d4b9e71d407c65be3a46f
ViktarKudasau <126377810+ViktarKudasau@users.noreply.github.com>
2023-03-08 22:43:08 +0300

    Delete HomeWork_1.java

--- src/HomeWork_1.java
+++
@@ -1,8 +0,0 @@
-public class HomeWork_1 {
-    public static void main(String[] args) {
-
-        System.out.println("Viktar\nKudasau");
-        System.out.println("Economist");
-        System.out.println("GYM");
-    }
-}
